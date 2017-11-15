5b740886c629aba481f6019e9d51ee4d44cb9e66
pmoreno <pmoreno@megamedia.es>
2017-11-15 15:54:45 +0100

    changed port development

--- server/httpServer.js
+++ server/httpServer.js
@@ -2,9 +2,10 @@ import base from 'base/';
 import envConf from './enviroment';
 
 const httpServer = app => {
+  let port = envConf.port || process.env.PORT || 80;
   app.listen(envConf.port, (err) => {
     if (err) return base.console.error(`${err}`);
-    base.console.success(`HTTP Server up on http://localhost:${envConf.port}`);
+    base.console.success(`HTTP Server up on http://localhost:${port}`);
   });
 };
 
