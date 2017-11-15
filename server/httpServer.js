b5adc8def50d35c407951731f6b389c2dcf6ade2
pmorenopina <pmorenopina@gmail.com>
2017-11-15 19:34:25 +0100

    changed host

--- server/httpServer.js
+++ server/httpServer.js
@@ -3,9 +3,9 @@ import envConf from './enviroment';
 
 const httpServer = app => {
   let port = process.env.PORT || 80;
-  app.listen(envConf.port, process.env.host, (err) => {
+  app.listen(envConf.port, '0.0.0.0', (err) => {
     if (err) return base.console.error(`${err}`);
-    base.console.success(`HTTP Server up on http://localhost:${port}`);
+    base.console.success(`HTTP Server up on http://0.0.0.0:${port}`);
   });
 };
 
