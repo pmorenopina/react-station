8cd22406fb54cd2e1d7d16b98b64eb938a45135c
pmorenopina <pmorenopina@gmail.com>
2017-11-30 22:55:44 +0100

    fixed loop video for mobile

--- src/app/containers/Ataraxia/containers/Main/index.jsx
+++ src/app/containers/Ataraxia/containers/Main/index.jsx
@@ -27,9 +27,8 @@ class Main extends Component {
             autoPlay={ true } 
             muted={ true } 
             playsInline={ true } 
-            src={ '/assets/videos/ataraxia/home_video.mp4' } 
           >
-            <source type={ "video/mp4" } src={ '/assets/videos/ataraxia/home_video.mp4' } />
+            <source type={ "video/mp4" } src={ '/assets/videos/ataraxia/home-video.mp4' } />
           </video>
         </div>
         <div className={ styles.container_next_events }>
