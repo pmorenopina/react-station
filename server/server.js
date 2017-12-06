095750413c2a09ac2a1782e527834805bee91a9b
pmorenopina <pmorenopina@gmail.com>
2017-12-06 12:24:48 +0100

    Deteled awake sever ping

--- server/server.js
+++ server/server.js
@@ -1,37 +1,26 @@
-import express from 'express';
+import express from 'express'
+import base from 'base'
+import httpServer from './httpServer'
+import applyStaticsPaths from './statics'
+import applyServerRouting from './routing'
+import applyEnvMiddleWare from './middleware'
 
-import base from 'base';
-import httpServer from './httpServer';
-import applyStaticsPaths from './statics';
-import applyServerRouting from './routing';
-import applyEnvMiddleWare from './middleware';
-
-const app = express();
+const app = express()
 
 const launchServer = () => {
   applyEnvMiddleWare(app)
     .then(() => {
-      base.console.info(`Checking static paths...`);
-      applyStaticsPaths(app);
+      base.console.info(`Checking static paths...`)
+      applyStaticsPaths(app)
     })
     .then(() => {
-      base.console.info(`Checking server routing...`);
-      applyServerRouting(app);
+      base.console.info(`Checking server routing...`)
+      applyServerRouting(app)
     })
     .then(() => {
-      base.console.info(`Setting up server...`);
-      httpServer(app);
+      base.console.info(`Setting up server...`)
+      httpServer(app)
     })
-    .then(() => {
-      var http = require("http");
-      setInterval(function() {
-          base.console.info(`Setting awake server...`);
-          http.get("http://react-station.herokuapp.com");
-      }, 1500000);
-    })
-    .catch((e) => {
-      base.console.error(`Server Error ${e}...`);
-    });
-};
+}
 
-launchServer();
+launchServer()
