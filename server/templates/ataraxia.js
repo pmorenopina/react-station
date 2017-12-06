4f3b9b5a2bc9a141506f2ee1873315062b236031
pmorenopina <pmorenopina@gmail.com>
2017-12-06 12:33:29 +0100

    Changed general metas

--- server/templates/ataraxia.js
+++ server/templates/ataraxia.js
@@ -7,7 +7,7 @@ export default function main(params) {
 
   if (params.routeMatch === '/events/' || params.routeMatch === '/events') {
     title += ' - Eventos'
-  } else if (params.routeMatch === '/djs/' || params.routeMatch === '/ataraxia/djs') {
+  } else if (params.routeMatch === '/djs/' || params.routeMatch === '/djs') {
     title += ' - Djs'
   } else if (params.routeMatch === '/media/' || params.routeMatch === '/media') {
     title += ' - Media'
@@ -46,40 +46,41 @@ export default function main(params) {
 
   return `
   <!doctype html>
-	<html lang="utf-8">
+  <html lang="es" xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml">
     <head>
-      <link rev="made" href="mailto:pmorenopina@gmail.com">
-      <link rel="manifest" href="/assets/favicon/ataraxia/manifest.json">      
-      <link rel="apple-touch-icon" sizes="57x57" href="/assets/favicon/ataraxia/apple-icon-57x57.png">
-      <link rel="apple-touch-icon" sizes="72x72" href="/assets/favicon/ataraxia/apple-icon-72x72.png">
-      <link rel="apple-touch-icon" sizes="76x76" href="/assets/favicon/ataraxia/apple-icon-76x76.png">
-      <link rel="apple-touch-icon" sizes="114x114" href="/assets/favicon/ataraxia/apple-icon-114x114.png">
-      <link rel="apple-touch-icon" sizes="120x120" href="/assets/favicon/ataraxia/apple-icon-120x120.png">
-      <link rel="apple-touch-icon" sizes="144x144" href="/assets/favicon/ataraxia/apple-icon-144x144.png">
-      <link rel="apple-touch-icon" sizes="152x152" href="/assets/favicon/ataraxia/apple-icon-152x152.png">
-      <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/ataraxia/apple-icon-180x180.png">
-      <link rel="icon" type="image/png" sizes="192x192"  href="/assets/favicon/ataraxia/android-icon-192x192.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/ataraxia/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="96x96" href="/assets/favicon/ataraxia/favicon-96x96.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/ataraxia/favicon-16x16.png">
-      <meta property="og:title" content="${ title }">
-      <meta property="og:description" content="Sesión de música electrónica">
-      <meta property="og:image" content="/assets/favicon/ataraxia/favicon-96x96.png">
-      <meta property="og:url" content="http://www.ataraxiaclub.com">
+      <link rev="made" href="mailto:pmorenopina@gmail.com" />
+      <link rel="manifest" href="/assets/favicon/ataraxia/manifest.json" />      
+      <link rel="apple-touch-icon" sizes="57x57" href="/assets/favicon/ataraxia/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/assets/favicon/ataraxia/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/assets/favicon/ataraxia/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="/assets/favicon/ataraxia/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/assets/favicon/ataraxia/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/assets/favicon/ataraxia/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/assets/favicon/ataraxia/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/ataraxia/apple-icon-180x180.png" />
+      <link rel="icon" type="image/png" sizes="192x192"  href="/assets/favicon/ataraxia/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/ataraxia/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/assets/favicon/ataraxia/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/ataraxia/favicon-16x16.png" />
+      <meta property="og:type" content="website" />      
+      <meta property="og:site_name" content="ΔTΔRΔXIΔ" />      
+      <meta property="og:title" content="${ title }" />
+      <meta property="og:description" content="Sesión de música electrónica. Entradas e información de eventos | ΔTΔRΔXIΔ" />
+      <meta property="og:image" content="/assets/favicon/ataraxia/favicon-96x96.png" />
+      <meta property="og:url" content="http://www.ataraxiaclub.com" />
       <meta name="viewport" content="width=device-width" />
-      <meta name="msapplication-TileColor" content="#ffffff">
-      <meta name="msapplication-TileImage" content="/assets/favicon/ataraxia/ms-icon-144x144.png">
-      <meta name="theme-color" content="#ffffff">
-      <meta http-equiv="Content-Type" content="text/html; ISO-8859-1">
-      <meta name="DC.Language" scheme="RFC1766" content="Spanish">
-      <meta name="author" content="Pablo Moreno">
-      <meta name="reply-to" content="pmorenopina@gmail.com">
-      <meta name="description" content="Sesión de música electrónica">
-      <meta name="keywords" content="ataraxia,club,session,sesion,musica,music,ataraxia,techno,deep,house,electronica,electronic,madrid,spain,javier,de la vega,rico,martinez,julio,cafe,la palma">
-      <meta name="Resource-type" content="Document">
-      <meta name="DateCreated" content="Mon, 20 November 2017 17:00:00 GMT+1">
-      <meta name="Revisit-after" content="7 days">
-      <meta name="robots" content="all">
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="msapplication-TileImage" content="/assets/favicon/ataraxia/ms-icon-144x144.png" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta http-equiv="Content-Type" content="text/html; ISO-8859-1" />
+      <meta name="DC.Language" scheme="RFC1766" content="Spanish" />
+      <meta name="author" content="Pablo Moreno" />
+      <meta name="reply-to" content="pmorenopina@gmail.com" />
+      <meta name="description" content="Sesión de música electrónica. Entradas e información de eventos | ΔTΔRΔXIΔ" />
+      <meta name="keywords" content="ataraxia,club,session,sesion,musica,music,ataraxia,techno,deep,house,electronica,electronic,madrid,spain,javier,de la vega,rico,martinez,julio,cafe,la palma" />
+      <meta name="DateCreated" content="Mon, 20 November 2017 17:00:00 GMT+1" />
+      <meta name="Revisit-after" content="7 days" />
+      <meta name="robots" content="index, follow" />
       <title>${ title }</title>
       ${ params.vendorScript }
       ${ params.style }
