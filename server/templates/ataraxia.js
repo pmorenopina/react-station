a65f9560f41f7d141aa8dbd5a39784bf98d74e74
pmoreno <pmoreno@megamedia.es>
2017-12-07 15:53:42 +0100

    Changed template for Ataraxiaclub

--- server/templates/ataraxia.js
+++ server/templates/ataraxia.js
@@ -49,31 +49,19 @@ export default function main(params) {
   <html lang="es" xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml">
     <head>
       <link rev="made" href="mailto:pmorenopina@gmail.com" />
-      <link rel="manifest" href="/assets/favicon/ataraxia/manifest.json" />      
-      <link rel="apple-touch-icon" sizes="57x57" href="/assets/favicon/ataraxia/apple-icon-57x57.png" />
-      <link rel="apple-touch-icon" sizes="72x72" href="/assets/favicon/ataraxia/apple-icon-72x72.png" />
-      <link rel="apple-touch-icon" sizes="76x76" href="/assets/favicon/ataraxia/apple-icon-76x76.png" />
-      <link rel="apple-touch-icon" sizes="114x114" href="/assets/favicon/ataraxia/apple-icon-114x114.png" />
-      <link rel="apple-touch-icon" sizes="120x120" href="/assets/favicon/ataraxia/apple-icon-120x120.png" />
-      <link rel="apple-touch-icon" sizes="144x144" href="/assets/favicon/ataraxia/apple-icon-144x144.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/assets/favicon/ataraxia/apple-icon-152x152.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/ataraxia/apple-icon-180x180.png" />
-      <link rel="icon" type="image/png" sizes="192x192"  href="/assets/favicon/ataraxia/android-icon-192x192.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/ataraxia/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/assets/favicon/ataraxia/favicon-96x96.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/ataraxia/favicon-16x16.png" />
-      <meta property="og:type" content="website" />      
-      <meta property="og:site_name" content="ΔTΔRΔXIΔ" />      
-      <meta property="og:title" content="${ title }" />
-      <meta property="og:description" content="Sesión de música electrónica. Entradas e información de eventos | ΔTΔRΔXIΔ" />
-      <meta property="og:image" content="/assets/favicon/ataraxia/favicon-96x96.png" />
-      <meta property="og:url" content="http://www.ataraxiaclub.com" />
-      <meta name="viewport" content="width=device-width" />
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="msapplication-TileImage" content="/assets/favicon/ataraxia/ms-icon-144x144.png" />
-      <meta name="theme-color" content="#ffffff" />
-      <meta http-equiv="Content-Type" content="text/html; ISO-8859-1" />
-      <meta name="DC.Language" scheme="RFC1766" content="Spanish" />
+      <link rel="manifest" href="http://www.ataraxiaclub.com/assets/favicon/ataraxia/manifest.json" />      
+      <link rel="apple-touch-icon" sizes="57x57" href="http://www.ataraxiaclub.com/assets/favicon/ataraxia/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="http://www.ataraxiaclub.com/assets/favicon/ataraxia/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="http://www.ataraxiaclub.com/assets/favicon/ataraxia/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="http://www.ataraxiaclub.com/assets/favicon/ataraxia/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="http://www.ataraxiaclub.com/assets/favicon/ataraxia/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="http://www.ataraxiaclub.com/assets/favicon/ataraxia/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="http://www.ataraxiaclub.com/assets/favicon/ataraxia/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="http://www.ataraxiaclub.com/assets/favicon/ataraxia/apple-icon-180x180.png" />
+      <link rel="icon" type="image/png" sizes="192x192"  href="http://www.ataraxiaclub.com/assets/favicon/ataraxia/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="http://www.ataraxiaclub.com/assets/favicon/ataraxia/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="http://www.ataraxiaclub.com/assets/favicon/ataraxia/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="http://www.ataraxiaclub.com/assets/favicon/ataraxia/favicon-16x16.png" />
       <meta name="author" content="Pablo Moreno" />
       <meta name="reply-to" content="pmorenopina@gmail.com" />
       <meta name="description" content="Sesión de música electrónica. Entradas e información de eventos | ΔTΔRΔXIΔ" />
@@ -81,6 +69,18 @@ export default function main(params) {
       <meta name="DateCreated" content="Mon, 20 November 2017 17:00:00 GMT+1" />
       <meta name="Revisit-after" content="7 days" />
       <meta name="robots" content="index, follow" />
+      <meta property="og:type" content="website" />      
+      <meta property="og:site_name" content="ΔTΔRΔXIΔ" />      
+      <meta property="og:title" content="${ title }" />
+      <meta property="og:description" content="Sesión de música electrónica. Entradas e información de eventos | ΔTΔRΔXIΔ" />
+      <meta property="og:image" content="http://www.ataraxiaclub.com/assets/favicon/ataraxia/favicon-96x96.png" />
+      <meta property="og:url" content="http://www.ataraxiaclub.com" />
+      <meta name="viewport" content="width=device-width" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="msapplication-TileImage" content="http://www.ataraxiaclub.com/assets/favicon/ataraxia/ms-icon-144x144.png" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta http-equiv="Content-Type" content="text/html; ISO-8859-1" />
+      <meta name="DC.Language" scheme="RFC1766" content="Spanish" />
       <title>${ title }</title>
       ${ params.vendorScript }
       ${ params.style }
