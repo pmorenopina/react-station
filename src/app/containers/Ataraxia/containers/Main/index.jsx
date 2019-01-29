e71fdcda435e8c0979e5a10ab515a58108c1e669
pmoreno <pmorenopina@gmail.com>
2019-01-29 19:31:32 +0100

    Added feb19 party

--- src/app/containers/Ataraxia/containers/Main/index.jsx
+++ src/app/containers/Ataraxia/containers/Main/index.jsx
@@ -179,7 +179,7 @@ class Main extends Component {
             />
           </div>
           <div className={styles.description}>
-            <h2 className={styles.title}>ATARAXIA JANUARY 2019</h2>
+            <h2 className={styles.title}>ATARAXIA FEBRUARY 2019</h2>
             <p dangerouslySetInnerHTML={{ __html: CONFIG.remember[0].description }} />
           </div>
         </div>
