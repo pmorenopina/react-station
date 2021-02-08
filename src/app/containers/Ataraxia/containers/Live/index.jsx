36c8640db3cc2270172eca8eacddc4bdcb554c07
Pablo Moreno <pmorenopina@gmail.com>
2021-02-08 21:42:51 +0100

    Added countdown live

--- src/app/containers/Ataraxia/containers/Live/index.jsx
+++ src/app/containers/Ataraxia/containers/Live/index.jsx
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import Countdown from 'react-countdown'
 
 // Styles
 import styles from './live.css'
@@ -9,7 +10,8 @@ class Live extends Component {
     super(props)
     this.state = {
       url_hidrated: null,
-      chat_url_hidrated: null
+      chat_url_hidrated: null,
+      loadComponent: false
     }
   }
 
@@ -18,13 +20,50 @@ class Live extends Component {
     const { url, chat_url } = live
     this.setState({
       url_hidrated: url,
-      chat_url_hidrated: chat_url+window.location.hostname
+      chat_url_hidrated: chat_url + window.location.hostname,
     })
+    if(live && live.countDown && live.countDown.time) {
+      this.setCountdown(live.countDown.time)
+    }
+  }
+
+  setCountdown = (time) => {
+    const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+    let birthday = time,
+      countDown = new Date(birthday).getTime(),
+      x = setInterval(function () {
+
+        let now = new Date().getTime(),
+          distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          let headline = document.getElementById("headline"),
+            countdown = document.getElementById("countdown"),
+            content = document.getElementById("content");
+
+          headline.innerText = "It's my birthday!";
+          countdown.style.display = "none";
+          content.style.display = "block";
+
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
   }
 
   render() {
-    const {live} = this.props
-    const {url_hidrated, chat_url_hidrated} = this.state
+    const { live } = this.props
+    const { url_hidrated, chat_url_hidrated, loadComponent } = this.state
     const liveTag = (live && live.url) ? (
       <div className={
         live && live.url && !live.chat_url ? styles.container_live : styles.container_live_chat
@@ -46,9 +85,11 @@ class Live extends Component {
           />
         ) : null}
       </div>
-    ) : (
-        <h3 className={styles.notAvailable}>Broadcast currently not available</h3>
-      )
+    ) : null
+
+    const notAvailable = (live && live.notAvailable) ? (
+      <h3 className={styles.notAvailable}>{live.notAvailable}</h3>
+    ) : null
 
     const disclaimerTag = (live && live.url) ? (
       <div className={styles.disclaimer}>
@@ -57,10 +98,26 @@ class Live extends Component {
       </div>
     ) : null
 
+    const countDown = (live && live.countDown) ? (
+      <div className={styles.container}>
+        <div id="countdown">
+          <ul>
+            <li><span id="days"></span>days</li>
+            <li><span id="hours"></span>Hours</li>
+            <li><span id="minutes"></span>Minutes</li>
+            <li><span id="seconds"></span>Seconds</li>
+          </ul>
+        </div>
+        <h3 id="headline">{live.countDown.text}</h3>
+      </div>
+    ) : null
+
     return (
       <main className={styles.container}>
         <h1 className={styles.titleMain}>LIVE STREAMING</h1>
         { liveTag}
+        { notAvailable}
+        { countDown}
         { disclaimerTag}
       </main>
     )
